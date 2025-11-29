0ede9f74b5f23dc032724138207871bff8235b71
Nour AL KOTOB <alkotobnour@gmail.com>
2025-11-29 20:51:37 +0100

    fix nx batch create recursion

--- batch/create.sh
+++ batch/create.sh
@@ -34,7 +34,7 @@ done
 rejectForbiddenFlags "$@"
 
 target=${1:?${_red}param 2: missing required file path from \'/default-domain/workspaces/\'.$_def}
-batch_id=${2:?${_red}param 1: batch Id required. Consider using $($NAME init)$_def}
+batch_id=${2:?${_red}param 1: batch Id required. Consider using ${NAME% *}.$_def}
 shift 2
 
 cmd="http://$NUXEO_URL/nuxeo/api/v1/upload/$batch_id$file_idx/execute/FileManager.Import --json"
